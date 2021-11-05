f1ea18406801de0aed200ec0c7ce235fa0759705
FlickerSoul <13360148+FlickerSoul@users.noreply.github.com>
2021-11-04 21:06:33 -0700

    feat: model edge

--- networkx/classes/components/edge.py
+++ networkx/classes/components/edge.py
@@ -6,8 +6,13 @@ from .base import ContentWrapper
 from .node import Node
 
 
-class Edge(ContentWrapper[Tuple[Node, Node, bool]]):
-    def __init__(self, u, v, directed: bool = False):
+class Edge(ContentWrapper[Tuple[Node, Node, bool, int | None]]):
+    """Edge wrapper for a networkx edge
+
+    dilemma: does Edge act like a pure proxy or an edge?
+    """
+
+    def __init__(self, u, v, directed: bool = False, key: int = None):
         if not isinstance(u, Node):
             u = Node(u)
         if not isinstance(v, Node):
@@ -16,10 +21,80 @@ class Edge(ContentWrapper[Tuple[Node, Node, bool]]):
         if not isinstance(directed, bool):
             raise ValueError("directed argument has to be bool")
 
-        super(Edge, self).__init__((u, v, directed))
+        if key is not None and not isinstance(key, int):
+            raise ValueError("key argument has to be None or an integer")
+
+        super(Edge, self).__init__((u, v, directed, key))
+
+        self._edge = u, v
+        self._directed = directed
+        self._key = key
+
+    @property
+    def content(self) -> Tuple:
+        return self._edge
+
+    @property
+    def is_directed(self) -> bool:
+        return self._directed
+
+    @property
+    def key(self) -> int | None:
+        return self._key
+
+    def count(self, *args, **kwargs):
+        return self.content.count(*args, **kwargs)
+
+    def index(self, *args, **kwargs):
+        return self.content.index(*args, **kwargs)
+
+    def __contains__(self, item) -> bool:
+        return item in self.content
+
+    def __eq__(self, item):
+        if isinstance(item, Edge):
+            return super().__eq__(item)
+        return self.content == item
+
+    def __getitem__(self, item):
+        return self.content[item]
+
+    def __getnewargs__(self, *args, **kwargs):
+        # TODO support pickle
+        raise NotImplementedError
+
+    def __ge__(self, item):
+        return self.content >= item
+
+    def __gt__(self, item):
+        return self.content > item
+
+    def __iter__(self):
+        return self.content.__iter__()
+
+    def __len__(self):
+        return len(self.content)
+
+    def __le__(self, item):
+        return self.content <= item
+
+    def __lt__(self, item):
+        return self.content < item
+
+    def __add__(self, other):
+        raise NotImplementedError
+
+    def __mul__(self, *args, **kwargs):
+        raise NotImplementedError
+
+    def __ne__(self, *args, **kwargs):
+        raise NotImplementedError
+
+    def __rmul__(self, *args, **kwargs):
+        raise NotImplementedError
 
     def __repr__(self):
-        return f"Edge({str(self.content[0])}{'->' if self.content[2] else '-'}{str(self.content[1])})"
+        return f"Edge({repr(self.content[0])}, {repr(self.content[1])}, {self.is_directed}, {self.key})"
 
     def __str__(self):
-        return f"{repr(self.content[0])}{'->' if self.content[2] else '-'}{repr(self.content[1])}"
+        return f"{str(self.content[0])} {'->' if self.is_directed else '-'} {str(self.content[1])}"
