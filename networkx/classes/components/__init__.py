dad292cc3ad8f62756d84bb6185c768d5877888b
FlickerSoul <13360148+FlickerSoul@users.noreply.github.com>
2021-11-13 15:37:47 -0800

    feat: export is_node and is_edge

--- networkx/classes/components/__init__.py
+++ networkx/classes/components/__init__.py
@@ -1,5 +1,12 @@
-from .node import Node
-from .edge import Edge
 from .base import GRAPHERY_TYPE_FLAG_NAME, GRAPHERY_TYPES
+from .node import Node, is_node
+from .edge import Edge, is_edge
 
-__all__ = ["Node", "Edge", "GRAPHERY_TYPE_FLAG_NAME", "GRAPHERY_TYPES"]
+__all__ = [
+    "GRAPHERY_TYPE_FLAG_NAME",
+    "GRAPHERY_TYPES",
+    "Node",
+    "is_node",
+    "Edge",
+    "is_edge",
+]
