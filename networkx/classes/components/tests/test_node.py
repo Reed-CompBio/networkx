f6ee15197c8d21677545ee9de7326a58486ccb52
FlickerSoul <13360148+FlickerSoul@users.noreply.github.com>
2021-11-13 12:32:47 -0800

    feat: mod node test

--- networkx/classes/components/tests/test_node.py
+++ networkx/classes/components/tests/test_node.py
@@ -1,12 +1,14 @@
 from ..node import Node
+from .test_base import WrapperTestBase
 import pytest
 
 
-class TestNode:
-    def test_none(self):
-        with pytest.raises(TypeError):
-            n = Node(None)
+class TestNode(WrapperTestBase):
+    wrapper_type = Node
 
-    def test_nesting(self):
-        with pytest.raises(TypeError):
-            n = Node(Node(1))
+    @pytest.mark.parametrize("content", [pytest.param(1), pytest.param("str")])
+    def test_built_in_immutables(self, content):
+        super(TestNode, self).test_built_in_immutables(content)
+
+    def test_user_defined_class(self, **_):
+        pass
