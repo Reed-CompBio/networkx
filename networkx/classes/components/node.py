4e6207807a2970097126b803386d81c6695249e1
FlickerSoul <13360148+FlickerSoul@users.noreply.github.com>
2021-11-13 12:32:38 -0800

    feat: change node

--- networkx/classes/components/node.py
+++ networkx/classes/components/node.py
@@ -1,17 +1,8 @@
 from __future__ import annotations
 
-from typing import TypeVar
-
-from .base import ContentWrapper
+from .base import ContentWrapper, collect_graphery_type
 
 
-_T = TypeVar("_T")
-
-
-class Node(ContentWrapper[_T]):
-    def __init__(self, node_value: _T) -> None:
-        if node_value is None:
-            raise TypeError("Node cannot accept None as content.")
-        if isinstance(node_value, Node):
-            raise TypeError("Node cannot nest Node Type as content")
-        super(Node, self).__init__(node_value)
+@collect_graphery_type
+class Node(ContentWrapper):
+    _graphery_type_flag = "Node"
