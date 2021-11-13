5a3a3252079884113086f841bde9942574533050
FlickerSoul <13360148+FlickerSoul@users.noreply.github.com>
2021-11-13 11:55:41 -0800

    export graphery type flag and type dict

--- networkx/classes/components/__init__.py
+++ networkx/classes/components/__init__.py
@@ -1,4 +1,5 @@
 from .node import Node
 from .edge import Edge
+from .base import GRAPHERY_TYPE_FLAG_NAME, GRAPHERY_TYPES
 
-__all__ = ["Node", "Edge"]
+__all__ = ["Node", "Edge", "GRAPHERY_TYPE_FLAG_NAME", "GRAPHERY_TYPES"]
