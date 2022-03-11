c266c4b29699290333dff5440e3c9e3029ec0341
FlickerSoul <13360148+FlickerSoul@users.noreply.github.com>
2022-03-10 20:26:40 -0800

    feat: bump up version

--- networkx/__init__.py
+++ networkx/__init__.py
@@ -8,7 +8,7 @@ structure, dynamics, and functions of complex networks.
 See https://networkx.org for complete documentation.
 """
 
-__version__ = "2.7.1r1"
+__version__ = "2.7.1r2"
 
 
 def __getattr__(name):
